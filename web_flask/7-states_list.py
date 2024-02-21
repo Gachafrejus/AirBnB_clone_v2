d0c9b67d1fb3a7d237ca7d6a959d163134747805
gachafrejus <frejuskinf@gmail.com>
2024-02-21 12:54:16 -0800

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-""" Flask web application """
-from flask import Flask, render_template
+"""Starts application.
+
+listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
 from models import storage
-from models.state import State
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    """ Display a list of all State objects sorted by name """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    """Show an HTML page with a list of all State objects in DBStorage.
+
+    States are sorted.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """ Remove the current SQLAlchemy Session """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
